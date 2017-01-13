13fce16a0bbfc7b19ca46cfe67725a39c0749549
Jakub Lattenberk <lattenberk.jakub@gmail.com>
2017-01-13 15:35:52 +0100

    heroes

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -12,7 +12,7 @@ export class HeroService {
 
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
-      setTimeout(() => resolve(this.getHeroes()), 2000)
+      setTimeout(() => resolve(this.getHeroes()), 200)
     })
   }
 
